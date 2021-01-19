47689b4e393e3c91cac9c7bacfe3ae173b6620b7
Nikolay Yuzefovich <33313888+k01ek@users.noreply.github.com>
2021-01-19 18:25:08 +0500

    Update template_content.py

--- netbox_qrcode/template_content.py
+++ netbox_qrcode/template_content.py
@@ -35,7 +35,7 @@ class QRCode(PluginTemplateExtension):
                 text.append(custom_text)
             text = '\n'.join(text)
             text_img = get_qr_text(qr_img.size, text, config.get('font'))
-            qr_with_text = get_concat(qr_img, text_img, config.get('textposition'))
+            qr_with_text = get_concat(qr_img, text_img, config.get('text_position'))
             img = get_img_b64(qr_with_text)
         else:
             img = get_img_b64(qr_img)
