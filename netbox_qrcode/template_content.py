0a354bf9b0603acd5a9276aa70088b16806c1566
LHBL2003 <Mister.Hempel@gmail.com>
2022-05-18 11:43:19 +0200

    If a font_size is specified in the configuration, this fixed font size is used.
    If the font_size is not specified or if the value = 0, then the previous dynamic behavior will continue to be executed.
    
    Issue #11 from the original is related to the topic.

--- netbox_qrcode/template_content.py
+++ netbox_qrcode/template_content.py
@@ -60,7 +60,7 @@ class QRCode(PluginTemplateExtension):
                 if custom_text:
                     text.append(custom_text)
                 text = '\n'.join(text)
-            text_img = get_qr_text(qr_img.size, text, config.get('font'))
+            text_img = get_qr_text(qr_img.size, text, config.get('font'), config.get('font_size', 0))
             qr_with_text = get_concat(qr_img, text_img, config.get('text_location', 'right'))
 
             img = get_img_b64(qr_with_text)
