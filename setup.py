2140c53b24d006971c4686c9dff64e72edba3523
Chris Russell <cruse1977123@gmail.com>
2025-12-16 22:48:52 +0000

    4.5-release-prep

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ setup(
     author_email='mgk.kolek@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    min_version='4.4.0',
-    max_version='4.4.99',
+    min_version='4.5.0-beta1',
+    max_version='4.5.99',
     package_data={
         '': ['*.ttf'],
         '': ['*.html'],
