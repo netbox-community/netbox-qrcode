96f89b5c0ce557b6fbb49af0ec0eb1717cb87aad
Chris Russell <cruse1977123@gmail.com>
2026-04-30 11:14:17 +0100

    version bumps

--- netbox_qrcode/__init__.py
+++ netbox_qrcode/__init__.py
@@ -10,8 +10,8 @@ class QRCodeConfig(PluginConfig):
     author = 'Nikolay Yuzefovich'
     author_email = 'mgk.kolek@gmail.com'
     required_settings = []
-    min_version = '4.5.0-beta1'
-    max_version = '4.5.99'
+    min_version = '4.6.0-beta1'
+    max_version = '4.6.99'
     default_settings = {
 	
         ################################## 
